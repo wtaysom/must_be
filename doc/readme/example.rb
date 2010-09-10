0a77bf501be47c83c483c62ba1679a0a7e675252
William Taysom <wtaysom@gmail.com>
2010-09-10 19:53:07 +0800

    Write basic examples.

--- doc/readme/example.rb
+++ doc/readme/example.rb
@@ -1,5 +1,110 @@
-#!! what are our examples?
+## Basic
 
-3.must == 3
-3.must == 4
-#=> 3.must.==(4)
+87.must_be_a(Float, Integer)
+87.must_be_a(Symbol, String)
+#=> 87.must_be_a(Symbol, String), but is a Fixnum
+
+87.must_not_be_a(Symbol, String, "message")
+87.must_not_be_a(Float, Integer, "message")
+#=> 87.must_not_be_a(Float, Integer, "message"), but is a Fixnum
+
+2.must_be_in(1, 2, 3)
+2.must_be_in([1, 2, 3])
+2.must_be_in(1 => 3, 2 => 4)
+2.must_be_in(1..3)
+2.must_be_in
+#=> 2.must_be_in
+
+2.must_not_be_in
+2.must_not_be_in(4, 5)
+2.must_not_be_in(1 => 3, 2 => 4)
+#=> 2.must_not_be_in({1=>3, 2=>4})
+
+true.must_be_boolean
+false.must_be_boolean
+nil.must_be_boolean
+#=> nil.must_be_boolean
+
+2.must_be_close(2.0)
+2.must_be_close(2.01)
+2.must_be_close(2.1)
+#=> 2.must_be_close(2.1, 0.1), difference is 0.1
+
+2.must_be_close(2.1, 6)
+2.must_be_close(9.0, 6)
+#=> 2.must_be_close(9.0, 6), difference is 7.0
+
+# must_be uses case (===) equality.
+34.must_be(Integer, :all)
+:all.must_be(Integer, :all)
+:none.must_be(Integer, :all)
+#=> :none.must_be(Integer, :all), but matches Symbol
+
+5.must_be(1..5)
+5.must_be(1...5)
+#=> 5.must_be(1...5), but matches Fixnum
+
+5.must_not_be(1...5)
+3.must_not_be(1...5)
+#=> 3.must_not_be(1...5), but matches Fixnum
+
+true.must_be
+nil.must_be
+#=> nil.must_be, but matches NilClass
+false.must_be
+#=> false.must_be, but matches FalseClass
+
+nil.must_not_be
+:hello.must_not_be
+#=> :hello.must_not_be, but matches Symbol
+
+:yep.must_be(lambda {|v| v == :yep})
+:nope.must_be(lambda {|v| v == :yep})
+#~> :nope.must_be\(#<Proc:0x[^.]+?>\), but matches Symbol
+
+:yep.must_not_be(lambda {|v| v == :nope})
+:nope.must_not_be(lambda {|v| v == :nope})
+#~> :nope.must_not_be\(#<Proc:0x[^.]+?>\), but matches Symbol
+
+## `attr_typed`
+
+#!!
+#attr_typed
+
+## Containers
+
+#!!
+#must_only_contain
+#must_not_contain
+#must_only_ever_contain
+#must_never_ever_contain
+
+## Proxy
+
+#!!
+#must {}
+#must ==
+#must >
+#must even?
+
+#must_not {}
+#must_not ==
+#must_not <
+#must_not odd?
+
+## Core
+
+#must_notify(string)
+#must_notify(object, message, args, block, other message)
+#must_notify(note)
+
+#must_check {}
+#must_check(proc) {}
+
+## Nonstandard Control Flow
+
+#!!
+#must_raise(Error, /match/) {}
+#must_not_raise {}
+#must_throw()
+#must_not_throw(:this, "object") {}
--- doc/readme/example_runner.rb
+++ doc/readme/example_runner.rb
@@ -1,18 +1,47 @@
 require 'lib/must_be'
 
+$expecting_note = false
+
+class ExpectedNoteError < StandardError; end
+class MismatchedNoteError < StandardError; end
+
 MustBe.notifier = lambda do |note|
+  unless $expecting_note
+    raise ExpectedNoteError
+  end
   $note = note
   false
 end
 
 def check_note(message)
-  unless message == $note.message
-    puts "expected: #{message}"
-    puts "   found: #{$note.message}"
-    raise "mismatch"
+  $expecting_note = true
+  $note = nil
+  yield
+  unless $note and message === $note.message
+    puts "expected: #{message.is_a?(Regexp) ? message.inspect : message}"
+    if $note
+      puts "   found: #{$note.message}"
+    else
+      puts "but did not notify"
+    end
+    raise MismatchedNoteError
   end
+ensure
+  $expecting_note = false
 end
 
 example_text = IO.read(File.dirname(__FILE__)+"/example.rb")
-example_text.gsub!(/^#=> (.*)$/, 'check_note(%{\1})')
-eval example_text
+example_text.gsub!(/^(.*)\n#=> (.*)$/, "check_note(%{\\2}) {\n\\1}")
+example_text.gsub!(/^(.*)\n#~> (.*)$/, "check_note(%r{\\2}) {\n\\1}")
+
+begin
+  eval example_text
+  puts "Examples are okay."
+rescue ExpectedNoteError => ex
+  eval_frame = ex.backtrace.last
+  eval_frame =~ /:(\d+)$/
+  eval_line = $1.to_i
+  offending_line = example_text.split($/)[eval_line - 1]
+  puts "example.rb:#{eval_line}: expected note: #{offending_line}"
+rescue MismatchedNoteError
+end
