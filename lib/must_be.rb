ca3299e257a1a0f8cf02002aac0a5b8c3b3bf253
William Taysom <wtaysom@gmail.com>
2010-09-10 18:34:06 +0800

    Make requires safer.

--- lib/must_be.rb
+++ lib/must_be.rb
@@ -1,9 +1,9 @@
-$LOAD_PATH << File.expand_path(File.dirname(__FILE__))
+here = File.expand_path(File.dirname(__FILE__))
 
-require 'must_be/core'
-require 'must_be/basic'
-require 'must_be/must_and_must_not'
-require 'must_be/containers'
-require 'must_be/containers_registered_classes'
-require 'must_be/attr_typed'
-require 'must_be/nonstandard_control_flow'
+require here+'/must_be/core'
+require here+'/must_be/basic'
+require here+'/must_be/proxy'
+require here+'/must_be/containers'
+require here+'/must_be/containers_registered_classes'
+require here+'/must_be/attr_typed'
+require here+'/must_be/nonstandard_control_flow'
